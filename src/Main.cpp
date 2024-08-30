08cb575e87c83b2229b8f22e1bde42a5dc2c8414
Msato-kmc <165989267+Msato-kmc@users.noreply.github.com>
2024-08-30 13:34:59 +0900

    json読み込み成功しました。

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,11 @@
 #include <Siv3D.hpp> // Siv3D v0.6.15
-#include <string.h>
+#include <string>
 #include <stdlib.h>
+
+#include <string> //read&write
+#include <fstream>
+#include <sstream>
+
 #include <vector>
 #include "json.hpp"
 #include "board_setting.cpp"
@@ -8,6 +13,7 @@
 #include "setting.hpp"
 //using namespace nlohman;
 using namespace std;
+using json = nlohmann::json;
 
 int BOARD_WIDTH;
 int BOARD_HEIGHT;
@@ -21,6 +27,36 @@ vector<vector<int>> search_block(vector<int>& board_now, vector<int>& board_fini
 vector<vector<int>> define_size();
 vector<vector<vector<int> > > define_nukigata(vector<vector<int>> size);
 vector<int> katanuki(int piece_num, int x_min, int y_min, int direction, vector<vector<int>>& size, vector<vector<vector<int>>>& nukigata, vector<int> board, int BOARD_WIDTH, int BOARD_HEIGHT);
+
+//jsonファイル読み込み系
+// 文字列からベクトルに変換する関数(一列分を返す)
+vector<int> stringToVector(const string &str)
+{
+    vector<int> row;
+    for (char c : str)
+    {
+        row.push_back(c - '0'); // 文字を整数に変換
+    }
+    return row;
+}
+
+// JSONからボードを読み込む関数
+int loadBoard(const json &jobj, vector<int> &startBoard, vector<int> &goalBoard)
+{
+    for (const auto &line : jobj["board"]["start"])
+    {
+        vector<int> row = stringToVector(line.get<string>()); //一列分をrowに格納
+        startBoard.insert(startBoard.end(), row.begin(), row.end()); //.insert()で一次元に変換。
+    }
+
+    for (const auto &line : jobj["board"]["goal"])
+    {
+        vector<int> row = stringToVector(line.get<string>());
+        goalBoard.insert(goalBoard.end(), row.begin(), row.end());
+    }
+    return 0;
+}
+
 
 
 void Main()
@@ -38,15 +74,13 @@ void Main()
     TextEditState te1{ U"0,0,0" };// デフォルトのテキストを設定する
     String entered_text;
 
-    BOARD_WIDTH = 20;
-    BOARD_HEIGHT = 20;
-
     vector<int> board_start (BOARD_WIDTH * BOARD_HEIGHT, 0);
 	vector<int> board_now (BOARD_WIDTH * BOARD_HEIGHT, 0);
     vector<int> board_finish (BOARD_WIDTH * BOARD_HEIGHT, 0);
     vector<vector<int>> blockcheck_result;
 
 	//board_setting(random)
+    /* 
 	for(int i = 0; i < BOARD_HEIGHT; i++){
 		for(int j = 0; j < BOARD_WIDTH; j++){
 			board_start.at(BOARD_WIDTH * i + j) = rand() % 4;
@@ -57,6 +91,49 @@ void Main()
 			board_finish.at(BOARD_WIDTH * i + j) = rand() % 4;
 		}
 	}
+     */
+    std::filesystem::current_path("../"); //カレントディレクトリをソースファイルに変更。
+    cout << "Current path: " << std::filesystem::current_path().c_str() << endl; //カレントディレクトリを表示;
+
+    //jsonファイル存在するか確認。
+    if (!std::filesystem::exists("./src/sample1.json")) {
+        std::cerr << "File does not exist at path: ./src/sample1.json" << std::endl;
+        return;
+    }
+
+    // JSONファイルの読み込み
+    std::ifstream ifs("./src/sample1.json");
+
+    //ファイル開け成功したか
+    if (!ifs.is_open()) {
+        std::cerr << "Failed to open the file!!!" << std::endl;
+        return;
+    } else {
+        std::cout << "File opened successfully!" << std::endl;
+    }
+
+    // ファイル内容を読み込み成功したか
+    std::string str((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+    if (str.empty()) {
+        std::cerr << "File is empty: ./src/sample1.json" << std::endl;
+        return;
+    }
+    //std::cout << "File content: " << str << std::endl; //中身表示
+
+    json jobj = json::parse(str);
+    std::cout << "JSON parsed successfully!" << std::endl;
+
+    //初期盤面、終了盤面取得
+    vector<int> startBoard, goalBoard;
+    loadBoard(jobj, startBoard, goalBoard); 
+    board_start = startBoard;
+    board_finish = goalBoard;
+
+    //幅と高さ取得
+    BOARD_WIDTH = jobj["board"]["width"].get<int>();
+    BOARD_HEIGHT = jobj["board"]["height"].get<int>();
+    cout << "WIDTH"<<BOARD_WIDTH << " HWIGHT"<<BOARD_HEIGHT << endl;
+
 
     board_now = board_start;
 
