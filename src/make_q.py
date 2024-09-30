f25923693a97f3133e98dc8cf586e32bb95a90f6
itogakuto <drycurry4444@gmail.com>
2024-09-30 10:44:27 +0900

    更新

--- src/make_q.py
+++ src/make_q.py
@@ -1,4 +1,3 @@
-# 画像を読み込む
 import cv2
 import numpy as np
 import pathlib
@@ -46,32 +45,31 @@ json_name = pathlib.PurePath(jpeg_name).stem + '.json'
 #jsonファイル出力       
 file = open(json_name, 'w')
 
-file.write('{\n')  
-file.write('    "board": {\n')
-file.write('        "width": ')
+file.write('"board": {\n')
+file.write('"width": ')
 file.write(str(x_div))
 file.write(',\n')
-file.write('        "height": ')
+file.write('"hight": ')
 file.write(str(y_div))
 file.write(',\n')
 
-file.write('        "start": [\n')
+file.write('"start": [\n')
 for y in range(y_div):
-    file.write('            "')
+    file.write('"')
     for x in range(x_div):
-        file.write(str(int(img_rnd_small[y][x] / 64)))
+        file.write(str(int(img_rnd_small[y][x]  / 64)))
         #file.write('x')
     file.write('"')
     if y < y_div - 1:
         file.write(',')
     file.write('\n')
 
-file.write('        ],\n')
+file.write('],\n')
 
 
-file.write('        "goal": [\n')
+file.write('"goal": [\n')
 for y in range(y_div):
-    file.write('            "')
+    file.write('"')
     for x in range(x_div):
         file.write(str(int(img_small[y][x] / 64)))
         #print(int(img_small[y][x] / 64), ' ', end='')
@@ -81,17 +79,42 @@ for y in range(y_div):
     file.write('\n')
     #print('')
 
-file.write('        ]\n')
-file.write('    }\n')
-file.write('}')
+file.write(']\n')
+file.write('},\n')
 
 file.close()
 
 print('Enterキーを押してください')
 
-# 画像をウィンドウで表示
-img_disp = cv2.hconcat([img_mos, img_rnd])
-img_disp = cv2.resize(img_disp, None, fx=0.8, fy=0.8)
+#以下は表示のための処理
+
+#4値化
+img_small_4value = np.copy(img_small)
+img_rnd_4value = np.copy(img_rnd_small)
+for y in range(y_div):
+    for x in range(x_div):
+        img_small_4value[y][x] = int(img_small[y][x] / 64) * 64 
+        img_rnd_4value[y][x] = int(img_rnd_small[y][x] / 64) * 64 
+img_small_4value = cv2.resize(img_small_4value, (w, h), interpolation=cv2.INTER_AREA)
+img_rnd_4value = cv2.resize(img_rnd_4value, (w, h), interpolation=cv2.INTER_AREA)
+
+# グレー画像とモザイク画像を横に並べる
+img_gray_rgb = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
+cv2.putText(img_gray_rgb, 'grayscale', (20, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 0), 2)
+img_mos_rgb = cv2.cvtColor(img_mos, cv2.COLOR_GRAY2BGR)
+cv2.putText(img_mos_rgb, 'mosaic', (20, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 0), 2)
+img_disp1 = cv2.hconcat([img_gray_rgb, img_mos_rgb])
+
+# ４値化したモザイクとランダム画像を横に並べる
+img_small_4value = cv2.cvtColor(img_small_4value, cv2.COLOR_GRAY2BGR)
+cv2.putText(img_small_4value, '4-value', (20, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 0), 2)
+img_rnd_4value = cv2.cvtColor(img_rnd_4value, cv2.COLOR_GRAY2BGR)
+cv2.putText(img_rnd_4value, 'randomize', (20, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 0), 2)
+img_disp2 = cv2.hconcat([img_small_4value, img_rnd_4value])
+
+#縦に並べて表示
+img_disp = cv2.vconcat([img_disp1, img_disp2])
+img_disp = cv2.resize(img_disp, None, fx=0.5, fy=0.5)
 cv2.imshow('Image', img_disp)
 
 # キーボード入力を待つ
