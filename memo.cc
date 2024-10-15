ff3df4cd4dcbb17dbd64f5378be0aeef364b3e9d
tarabakz25 <aikikizuki@gmail.com>
2024-10-15 19:43:25 +0900

    一旦コミット

--- memo.cc
+++ memo.cc
@@ -1,30 +1,15 @@
 #include <bits/stdc++.h>
-using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int a = 0;
+    int b = 1;
 
-    vector<vector<char>> arr(n, vector<char>(n));
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> arr[i][j];
-        }
+    if(a > b){
+        cout << "aa" << endl;
+    }else{
+        cout << "bb" << endl;
     }
 
-    for(int a = 0; a < n/2; a ++){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                swap(arr[j][i], arr[i][n + 1 - j]);
-            }
-        }
-    }
-
-    for(int  i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cout << arr[i][j];
-        }
-        cout << endl;
-    }
+    return 0;
 }
