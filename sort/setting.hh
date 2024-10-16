1c8c97db9f5e477803342f93aca4f65f2284844a
tarabakz25 <aikikizuki@gmail.com>
2024-10-16 19:19:24 +0900

    一旦コミット

--- sort/setting.hh
+++ sort/setting.hh
@@ -1,4 +1,4 @@
 #define DEBUG 0 //赤文字の表示
 #define PRINTING 0 // 盤面の表示
-#define SORT_FILE "problem_16x16.json" // サイズの設定
+#define SORT_FILE "problem_offical.json" // サイズの設定
 #define SLEEP_INTERVAL 0 // 遅延(秒)
